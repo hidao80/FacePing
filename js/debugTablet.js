43e5108ebdf7e0cbf1304e748941a405300d6706
hidao80 <hidao80@users.noreply.github.com>
2021-07-12 05:06:28 +0900

    fix: debug領域をdetails要素に変更。スタイルは css で指定するようにした

--- js/debugTablet.js
+++ js/debugTablet.js
@@ -4,7 +4,8 @@
  *  Usage:
  *     import debug from "./debugTablet.js";
  *     It is also useful to control on/off with the url parameter.
- *     debug.init(true);       // Debugging screen enabled
+ *     const DEUBG = new URL(window.location.href).searchParams.get('DEBUG') ? true : false;  // Setting the debug flag
+ *     debug.init(DEUBG);       // Debugging screen enabled
  *     debug.alignRight(true); // Right-aligned (left-aligned by default)
  *
  *  Licence: MIT
@@ -19,45 +20,82 @@
  */
 const debug = {
     // Output area
-    pane: document.createElement("div"),
+    div: document.createElement("div"),
+    details: document.createElement("details"),
 
     // Output logs
     log: msg => {
         const str = typeof(msg) == "string" ? msg : JSON.stringify(msg,undefined,1);
-        debug.pane.insertAdjacentHTML("beforeend", `<p>${str}</p><hr>`);
-        debug.pane.scrollTop = debug.pane.scrollHeight;
+        debug.div.insertAdjacentHTML("beforeend", `<p>${str}</p><hr>`);
+        debug.div.scrollTop = debug.div.scrollHeight;
     },
 
     // Right-aligned (left-aligned by default)
-    alignRight: pos => {
-        if (pos) {
-            debug.pane.style.right = 0;
-            debug.pane.style.left = null;
+    alignRight: (condition) => {
+        if (condition) {
+            debug.details.style.right = 0;
+            debug.details.style.left = null;
         } else {
-            debug.pane.style.right = null;
-            debug.pane.style.left = 0;
+            debug.details.style.right = null;
+            debug.details.style.left = 0;
         }
     },
 
     // Enabling the debug function
     init: isDisplay => {
-        debug.pane.style.display = "none";
-        debug.pane.style.position = "absolute";
-        debug.pane.style.backgroundColor = "gray";
-        debug.pane.style.color = "white";
-        debug.pane.style.top = 0;
-        debug.pane.style.left = 0;
-        debug.pane.style.height = "-webkit-fill-available";
-        debug.pane.style.width = "350px";
-        debug.pane.style.opacity = 0.6;
-        debug.pane.style.zIndex = 2000;
-        debug.pane.style.overflow = "scroll";
+        const summary = document.createElement("summary");
+        summary.textContent = "Details: ";
 
-        document.body.appendChild(debug.pane);
+        const moveOpposite = document.createElement("a");
+        moveOpposite.insertAdjacentHTML("beforeend", "Move to the Opposite");
+        moveOpposite.addEventListener("click", () => {
+            debug.alignRight(debug.details.style.left == "0px");
+        });
+        summary.appendChild(moveOpposite);
+
+        debug.div.setAttribute("id", "debug-div");
+        debug.details.setAttribute("id", "debug-details");
+        moveOpposite.setAttribute("id", "debug-opposite");
+
+        debug.details.appendChild(summary);
+        debug.details.appendChild(debug.div);
+
+        document.body.appendChild(debug.details);
+
+        document.head.insertAdjacentHTML("beforeend", `
+        <style>
+        #debug-details {
+            display: none;
+            position: absolute;
+            top: 0;
+            width: 350px;
+            opacity: 0.6;
+            z-index: 2000;
+            background-color: gray;
+            color: white;
+        }
+        #debug-details[open] {
+            height: -webkit-fill-available;
+            max-height: -webkit-fill-available;
+        }
+        #debug-div {
+            height: calc(100% - 1.2rem);
+            background-color: gray;
+            color: white;
+            overflow: scroll;
+        }
+        #debug-opposite {
+            cursor: pointer;
+            color: blue;
+            font-weight: 800;
+        }
+        </style>
+        `);
 
         // Whether to display or not
         if (isDisplay) {
-            debug.pane.style.display = "block";
+            debug.details.style.display = "block";
+            debug.details.style.left = 0;
             console.log = debug.log;  // Overwrite console.log
         }
     }
